28275663bbdd648f501d1b0cf3a7e496051f8f0f
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2024-09-13 12:34:18 +0200

    chore: cleanup

--- test/cypress/e2e/landing.cy.js
+++
@@ -1,9 +0,0 @@
-/* global cy */
-describe('The landing page', function () {
-  it.skip ('should load ', function () {
-    cy.visit('/exist/apps/jgoethe/index.html')
-      .get('.alert')
-      .contains('app.xqm')
-  })
-
-})
